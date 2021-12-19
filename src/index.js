3c7e0553fd2f68d144526ec7add6729f4af61ecf
SteRap <92691510+SteRap@users.noreply.github.com>
2021-12-19 19:06:40 +0100

    fixing routing issues

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename="/save-the-meal">
     <App />
   </HashRouter>,
   document.getElementById("root")
